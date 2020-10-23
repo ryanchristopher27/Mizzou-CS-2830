640ae33ed67cdd63331096734387e84f19c9d9be
ryanchristopher27 <ryandchristopher27@gmail.com>
2020-10-23 16:29:50 -0500

    2830 updates
    2830 lessons and challenges

---
+++ Lecture14/Building Apps 2 (During Class)/PhotoViewer/app.js
@@ -0,0 +1,70 @@
+/*
+	Created by Professor Wergeles for CS2830 at the University of Missouri 
+
+	The following is the data used by the application.
+	Typically this data is loaded from a server or
+	from a file.
+	
+	To make life easier and to see how the app works, the 
+	data is provided here instead of loading it from somewhere.
+	
+*/
+
+var title = 'Animal Photographs';
+
+var photos = [
+	{
+		uri : '../images/birdnest.png',
+		title : 'Bird Nest'
+	},
+	{
+		uri : '../images/birdrow.png',
+		title : 'Row of Birds'
+	},
+	{
+		uri : '../images/bluefootedbird.png',
+		title : 'Blue-footed Birds'
+	},
+	{
+		uri : '../images/deer.png',
+		title : 'Running Deer'
+	},
+	{
+		uri : '../images/hounds.png',
+		title : 'Hound Dogs'
+	},
+	{
+		uri : '../images/parrots.png',
+		title : 'Colorful Parrots'
+	},
+	{
+		uri : '../images/penguins.png',
+		title : 'Rockhopper Penguins'
+	},
+	{
+		uri : '../images/pheasants.png',
+		title : 'Pheasants'
+	},
+	{
+		uri : '../images/polarbears.png',
+		title : 'Polar Bears'
+	},
+	{
+		uri : '../images/sheep.png',
+		title : 'Flock of Sheep'
+	}
+];
+
+// The following 'controller' code utilizes the above data.
+// The code and the html in index.html do not contain any specifics
+// of the images that are displayed.  The data 'drives' what the logic does.
+
+// The paradigm is Model-View-Controller (MVC)
+// 		Model = the data above
+// 		View = interface elements and CSS
+// 		Controller = the code below
+// 	https://developer.chrome.com/apps/app_frameworks
+
+
+
+
---
+++ Lecture9/JavaScript and DOM (During Class)/2. Javascript/app.js
@@ -0,0 +1,5 @@
+/* This file was created by Ryan Christopher */
+
+console.log("External: Howdy!");
+
+window.alert("External: Howdy!");
